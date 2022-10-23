97193fce8c54f31f47e870b94088e2e2e6e46293
heriec <2821188630@qq.com>
2022-10-23 09:11:06 +0800

    修改原作者的版本，以及使用

--- analysis.py
+++ analysis.py
@@ -1,57 +1,102 @@
-from aip import AipNlp
-import pandas as pd
-import numpy as np
-import time
+import random
+import xlrd
+import excelSave as save
+import os
+import jieba
+from snownlp import SnowNLP
 
 
-# 此处输入baiduAIid
-APP_ID = ''
-API_KEY = ''
-SECRET_KEY = ''
+# 插入文本到txt中
+def insertToTxt(content, fileName):
+    with open(fileName, "a", encoding='utf-8') as file:
+        file.write(content + ",")
 
-client = AipNlp(APP_ID, API_KEY, SECRET_KEY)
 
-def isPostive(text):
+# 插入xls
+def insert_data(elems, sentiments_temp):
+    path = "test4.xls"
+    for index, elem in enumerate(elems):
+        workbook = xlrd.open_workbook(path)  # 打开工作簿
+        sheets = workbook.sheet_names()  # 获取工作簿中的所有表格
+        worksheet = workbook.sheet_by_name(sheets[0])  # 获取工作簿中所有表格中的的第一个表格
+        rows_old = worksheet.nrows  # 获取表格中已存在的数据的行数
+        rid = rows_old
+        # 热词
+        word = elem[0]
+        # 词频
+        frequency = elem[1]
+        # 情绪
+        emotion = sentiments_temp[index]
+
+        if frequency >= 3 and float(emotion) >= 0.7:
+            value1 = [
+                [rid, word, frequency, emotion], ]
+            print("当前插入第%d条数据" % rid)
+            save.write_excel_xls_append_norepeat("test4.xls", value1)
+
+
+file = open('test2.txt', errors="ignore", encoding="utf-8")
+file_context = file.read()
+words1 = jieba.lcut(file_context)  # 全模式
+words2 = jieba.lcut_for_search(file_context)  # 搜索引擎模式
+
+# 统计词频
+data1 = {}
+for chara in words1:
+    if len(chara) < 2:
+        continue
+    if chara in data1:
+        data1[chara] += 1
+    else:
+        data1[chara] = 1
+data1 = sorted(data1.items(), key=lambda x: x[1], reverse=True)  # 排序
+
+data2 = {}
+for chara in words2:
+    if len(chara) < 2:
+        continue
+    if chara in data2:
+        data2[chara] += 1
+    else:
+        data2[chara] = 1
+data2 = sorted(data2.items(), key=lambda x: x[1], reverse=True)  # 排序
+
+print("----------")
+
+# 放存入txt的，词语*频次列表
+txt_context = []
+# 放存入txt的，情感词列表
+sentiments_temp = []
+
+print("---褒义---")
+print('词语：           频次：       情感值：')
+for i in data1:
+    s1 = SnowNLP(i[0])
+    sentiments_temp.append(str(round(s1.sentiments, 2)))
     try:
-        if client.sentimentClassify(text)['items'][0]['positive_prob']>0.5:
-            return "积极"
-        else:
-            return "消极"
-    except:
-        return "积极"
+        if i[1] >= 10 and s1.sentiments >= 0.7:
+            # 打印
+            print(i[0] + '           ' + str(i[1]) + '              ' + str(round(s1.sentiments, 2)))
+            # 存txt
+            for j in range(i[1]):
+                txt_context.append(str(i[0]))
+    except ValueError:
+        pass
 
-# 读取文件，注意修改文件路径
-file_path = 'mlxg.xls'
-data = pd.read_excel(file_path,encoding='utf-8')
+# 存txt
+random.shuffle(txt_context)
+txt_str = ''
+for i in txt_context:
+    txt_str = txt_str + i + "，"
+insertToTxt(txt_str, 'test3.txt')
+print("存txt，ok")
 
-moods = []
-count = 1
-for i in data['微博内容']:
-    moods.append(isPostive(i))
-    count+=1
-    print("目前分析到："+count)
-
-data['情感倾向'] = pd.Series(moods)
-
-# 此处为覆盖保存
-data.to_excel(file_path)
-print("分析完成，已保存")
-
-
-'''
-# 此处为简单分类:P
-def fenlei(text):
-    xf = ['抽奖',"抽一个","抽一位","买","通贩"]
-    cz = ["画","实物","返图","合集","摸鱼","漫","自制","攻略","授权","草稿","绘"]
-    gj = ["hz","狗粉丝","狗女儿"]
-    for j in cz:
-        if j in text:
-            return "创作"
-    for i in xf:
-        if i in text:
-            return "消费"
-    for k in gj:
-        if k in text:
-            return "攻击"
-    return "其他"        
-''' 
+# 存xls
+if os.path.exists("test4.xls"):
+    print("文件已存在")
+else:
+    print("文件不存在，重新创建")
+    value_title = [["rid", "热词", "词频", "情感值"], ]
+    save.write_excel_xls("test4.xls", "统计数据", value_title)
+insert_data(data1, sentiments_temp)
+print("存xls，ok")
